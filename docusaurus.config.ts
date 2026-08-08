6afa79d8aa551782db5229983dc36f64bb95c8a5
Fade <beatsbystacking@gmail.com>
2026-08-09 00:41:54 +0200

    Add custom domain and vote page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,17 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://fade033.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/nationmc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Fade033', // Usually your GitHub org/user name.
+  projectName: 'nationmc', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
---
+++ src/pages/Vote.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Vote() {
+  return (
+    <Layout title="Vote" description="Vote for NationMC">
+      <main style={{padding: '2rem', textAlign: 'center'}}>
+        <h1>Vote for NationMC</h1>
+        <p>Voting page coming soon!</p>
+      </main>
+    </Layout>
+  );
+}
